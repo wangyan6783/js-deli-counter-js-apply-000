205e2d7884f134a879d09c8f222ef52f9460eb08
Yan Wang <wangyan6783@gmail.com>
2018-05-22 14:24:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 var katzDeli = [];
 var otherDeli = ["Steven", "Blake", "Avi"];
 
+<<<<<<< HEAD
 function takeANumber(array, name) {
   array.push(name);
   var number = array.length;
   return "Welcome, " + name + ". You are number " + number + " in line.";
+=======
+function takeANumber(array, string) {
+  array.push(string);
+  var index = array.indexOf(string) + 1;
+  return "Welcome, " + string + ". You are number " + index + " in line.";
+>>>>>>> 856dfd2621c6909061707d84914b30b6e4d64570
 }
 
 function nowServing(array) {
@@ -21,6 +28,7 @@ function currentLine(array) {
   if (array.length === 0) {
     return "The line is currently empty.";
   }
+<<<<<<< HEAD
   var currentLine = "The line is currently: ";
   for (var i = 0; i < array.length; i++) {
     var number = i + 1;
@@ -28,4 +36,13 @@ function currentLine(array) {
     currentLine += number + ". " + name + ", ";
   }
   return currentLine.slice(0, -2);
+=======
+  var line = "The line is currently: ";
+  for (var i = 0; i < array.length; i++) {
+    var number = i + 1;
+    var name = array[i];
+    line += number + ". " + name + ", ";
+  }
+  return line.slice(0, -2);
+>>>>>>> 856dfd2621c6909061707d84914b30b6e4d64570
 }
